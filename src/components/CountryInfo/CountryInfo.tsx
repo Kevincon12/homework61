4c55658761b4ab75d0b19f11d3db7afa09d56f95
Strelnikov Ilia <ily_strelnikov@mail.ru>
2025-12-25 15:35:38 +0600

    Дополнили вывод названий соседних стран если они есть.

--- src/components/CountryInfo/CountryInfo.tsx
+++ src/components/CountryInfo/CountryInfo.tsx
@@ -15,11 +15,21 @@ const CountryInfo: React.FC<Props> = ({id}) => {
             const response = await axios<CountryData>(`https://restcountries.com/v2/alpha/${id}`);
             const countryData = response.data;
 
+            let borderCountriesName : string[] = [];
+            if (countryData.borders?.length) {
+                const codes = countryData.borders.join(',');
+                const bordersResponse = await axios(`https://restcountries.com/v2/alpha?codes=${codes}`);
+                const bordersResponseData = bordersResponse.data;
+
+                const bordersToShow = bordersResponseData.map((country) => country.name);
+                borderCountriesName = bordersToShow;
+            }
+
             const newCountryData: CountryData = {
                 countryName: countryData.name,
                 capital: countryData.capital,
                 population: countryData.population,
-                borders: countryData.borders,
+                borders: borderCountriesName,
             }
             setCountry(newCountryData);
 
@@ -41,7 +51,12 @@ const CountryInfo: React.FC<Props> = ({id}) => {
                     <h1 className='card-title mp-1'>Country: {country.countryName}</h1>
                     <p className='card-body'>Capital: {country.capital}</p>
                     <p className='card-body'>Population: {country.population}</p>
-                    <span className='card-text'>Borders: {country.borders}</span>
+                    <span
+                        className='card-text'
+                        style={{color: country.borders && country.borders.length > 0 ? 'black' : 'red'}}
+                    >
+                        Borders: {country.borders && country.borders.length > 0 ? country.borders?.join(', '): 'No borders'}
+                    </span>
                 </>
             )}
         </div>
